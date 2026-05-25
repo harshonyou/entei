86b4def0b2efd9595f30e9f585db0398e3f2f685
Harsh Mohan <harshmohan813@gmail.com>
2026-05-25 12:39:02 +0100

    add env map

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 
 	"entei/evaluator"
+	"entei/object"
 	"entei/parser"
 
 	"entei/lexer"
@@ -15,6 +16,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -33,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			_, _ = io.WriteString(out, evaluated.Inspect())
 			_, _ = io.WriteString(out, "\n")
